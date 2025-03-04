9b1986d241b88abb6973cb8281919a18ac616b36
DELL <DELL@DESKTOP-7C911OD>
2025-03-04 14:35:33 +0530

    Values changed

--- src/test/java/com/nxtgenai/objectrepositoryz/dsvfdsvvvs.java
+++ src/test/java/com/nxtgenai/objectrepositoryz/dsvfdsvvvs.java
@@ -5,8 +5,8 @@ public class dsvfdsvvvs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=100;
-		int b=500;
+		int a=104520;
+		int b=5004512;
 		System.out.println(a+b);
 	}
 
